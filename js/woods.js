fc00fd847be952764f407821e24b12a78cbfe0a3
Steven Cobb <stevecobb91@gmail.com>
2017-03-13 22:52:43 -0400

    handle item ex

--- js/woods.js
+++ js/woods.js
@@ -5,9 +5,9 @@
 function checkWoods(object) {
   if (camera.position.z > 12) { //bottom
     if (hasKey == true) {
-      //window.location = "http://lost-woods.com/?hasKey=true";
+      window.location = "http://lost-woods.com/?hasKey=true";
     } else {
-      //window.location = "http://lost-woods.com";
+      window.location = "http://lost-woods.com";
     }
   }
   if (camera.position.x < -12) { //left
@@ -16,7 +16,7 @@ function checkWoods(object) {
   if (camera.position.x > 12) { //right
     
   }
-  if (camera.position.z < -8) { //top
+  if (camera.position.z < -12) { //top
     
   }
 
